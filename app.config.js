474c2aad5769383ef559687307f3e201d6a096e7
kovalenkoandri <kovalenkoandrii@gmail.com>
2024-01-03 00:19:37 +0200

    config

--- app.config.js
+++ app.config.js
@@ -3,8 +3,9 @@ module.exports = ({ config }) => {
   return {
     ...config,
     extra: {
-      eas: {},
-
+      eas: {
+        projectId: "9b09ed7a-525a-4896-81f2-7004336a3638",
+      },
       supabaseUrl: process.env.SUPABASE_URL,
       supabaseKey: process.env.SUPABASE_KEY,
     },
